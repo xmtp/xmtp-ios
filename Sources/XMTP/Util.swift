344f68865437fb3fb5c9c942c31eeae706777b8b
Pat Nakajima <patnakajima@gmail.com>
2022-11-21 12:35:29 -0800

    Remove un-used digest

--- Sources/XMTP/Util.swift
+++ Sources/XMTP/Util.swift
@@ -9,10 +9,6 @@ import CryptoSwift
 import Foundation
 
 enum Util {
-	static func keccak256Digest(_ data: Data) -> Keccak256Digest {
-		return Keccak256Digest(keccak256(data).bytes)
-	}
-
 	static func keccak256(_ data: Data) -> Data {
 		return Data(SHA3(variant: .keccak256).calculate(for: data.bytes))
 	}
