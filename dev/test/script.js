e08a965f1c269afbd82b2fe233184662ee70611d
Pat Nakajima <patnakajima@gmail.com>
2022-12-12 16:45:37 -0800

    Don't use network host

--- dev/test/script.js
+++ dev/test/script.js
@@ -1,6 +1,8 @@
 let Client = require("@xmtp/xmtp-js").Client;
 let Wallet = require("ethers").Wallet;
 
+console.log("NODE VERSION", process.version);
+
 // 0xf4BF19Ed562651837bc11ff975472ABd239D35B5
 const keyBytes = [
   80, 7, 53, 52, 122, 163, 75, 130, 199, 86, 216, 14, 29, 2, 255, 71, 121, 51,
@@ -9,7 +11,9 @@ const keyBytes = [
 
 async function checkAll() {
   const wallet = new Wallet(keyBytes);
-  const client = await Client.create(wallet, { env: "local" });
+  const client = await Client.create(wallet, {
+    apiUrl: "http://waku-node:5555",
+  });
   const stream = await client.conversations.stream();
 
   console.log("Listening…");
