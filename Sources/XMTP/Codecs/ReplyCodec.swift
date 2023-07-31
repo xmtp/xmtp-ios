92fc5f87f21a6c1f99a5d21665549206dd8bdbce
Naomi Plasterer <naomi@xmtp.com>
2023-07-31 09:51:42 -0700

    trying to make it codecable

--- Sources/XMTP/Codecs/ReplyCodec.swift
+++ Sources/XMTP/Codecs/ReplyCodec.swift
@@ -10,29 +10,31 @@ import Foundation
 
 public let ContentTypeReply = ContentTypeID(authorityID: "xmtp.org", typeID: "reply", versionMajor: 1, versionMinor: 0)
 
-public struct Reply: Codable {
+public struct Reply<T> {
     public var reference: String
-    public var content: Any
+    public var content: T
     public var contentType: ContentTypeID
 }
 
-public struct ReplyCodec: ContentCodec {
-    public typealias T = Reply
+public struct ReplyCodec<Content>: ContentCodec {
+    public typealias T = Reply<Content>
     public var contentType = ContentTypeReply
 
     public init() {}
 
-    public func encode(content: Reply) throws -> EncodedContent {
+    public func encode(content: Reply<Content>) throws -> EncodedContent {
         var encodedContent = EncodedContent()
 
         encodedContent.type = ContentTypeReply
-        encodedContent.content = try JSONEncoder().encode(content)
+        let codec = Client.codecRegistry.find(for: contentType)
+
+        encodedContent.content = try codec.encode(content: content).serializedData()
 
         return encodedContent
     }
 
-    public func decode(content: EncodedContent) throws -> Reply {
-        let reply = try JSONDecoder().decode(Reply.self, from: content.content)
+    public func decode(content: EncodedContent) throws -> Reply<Content> {
+        let reply = try JSONDecoder().decode(Reply<Content>.self, from: content.content)
         return reply
     }
 }
