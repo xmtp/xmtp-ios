cabfedde043728001160ca4cb056047939158da6
Naomi Plasterer <naomi@xmtp.com>
2023-08-01 11:58:33 -0700

    remove codable of content type id

--- Sources/XMTP/Codecs/ContentTypeID.swift
+++ Sources/XMTP/Codecs/ContentTypeID.swift
@@ -26,28 +26,3 @@ extension ContentTypeID {
 		"\(authorityID)/\(typeID):\(versionMajor).\(versionMinor)"
 	}
 }
-
-extension ContentTypeID: Codable {
-    enum CodingKeys: CodingKey {
-        case authorityID, typeID, versionMajor, versionMinor
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-
-        try container.encode(authorityID, forKey: .authorityID)
-        try container.encode(typeID, forKey: .typeID)
-        try container.encode(versionMajor, forKey: .versionMajor)
-        try container.encode(versionMinor, forKey: .versionMinor)
-    }
-
-    public init(from decoder: Decoder) throws {
-        self.init()
-
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        authorityID = try container.decode(String.self, forKey: .authorityID)
-        typeID = try container.decode(String.self, forKey: .typeID)
-        versionMajor = try container.decode(UInt32.self, forKey: .versionMajor)
-        versionMinor = try container.decode(UInt32.self, forKey: .versionMinor)
-    }
-}
