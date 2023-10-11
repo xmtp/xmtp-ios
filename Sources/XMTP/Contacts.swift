0d00b17a6783ee8037e371967b4f3d2af9ff7340
Pat Nakajima <patnakajima@gmail.com>
2023-10-11 12:41:15 -0700

    Start on v2 allow state stuff

--- Sources/XMTP/Contacts.swift
+++ Sources/XMTP/Contacts.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
+public enum AllowState: String, Codable {
+	case allowed, blocked, unknown
+}
+
+struct AllowListEntry: Codable, Hashable {
+	enum EntryType: String, Codable {
+		case address
+	}
+
+	static func address(_ address: String, type: AllowState) -> AllowListEntry {
+		AllowListEntry(value: address, entryType: .address, permissionType: type)
+	}
+
+	var value: String
+	var entryType: EntryType
+	var permissionType: AllowState
+}
+
+struct AllowList {
+	var allowedAddresses: Set<AllowListEntry> = []
+	var blockedAddresses: Set<AllowListEntry> = []
+
+	var entries: Set<AllowListEntry> = []
+
+	func state(address: String) -> AllowState {
+		entries.first(where: { $0.entryType == .address && $0.value == address })?.permissionType ?? AllowState.unknown
+	}
+}
+
 /// Provides access to contact bundles.
 public actor Contacts {
 	var client: Client
@@ -17,8 +46,48 @@ public actor Contacts {
 	// Whether or not we have sent invite/intro to this contact
 	var hasIntroduced: [String: Bool] = [:]
 
+	var allowList = AllowList()
+
 	init(client: Client) {
 		self.client = client
+	}
+
+	public func isAllowed(_ address: String) -> Bool {
+		for entry in allowList.entries {
+			switch entry.entryType {
+			case .address:
+				if address == entry.value {
+					return entry.permissionType == .allowed
+				}
+			}
+		}
+
+		return false
+	}
+
+	public func isBlocked(_ address: String) -> Bool {
+		for entry in allowList.entries {
+			switch entry.entryType {
+			case .address:
+				if address == entry.value {
+					return entry.permissionType == .blocked
+				}
+			}
+		}
+
+		return false
+	}
+
+	public func allow(addresses: [String]) {
+		for address in addresses {
+			allowList.entries.insert(.address(address, type: .allowed))
+		}
+	}
+
+	public func block(addresses: [String]) {
+		for address in addresses {
+			allowList.entries.insert(.address(address, type: .blocked))
+		}
 	}
 
 	func markIntroduced(_ peerAddress: String, _ isIntroduced: Bool) {
