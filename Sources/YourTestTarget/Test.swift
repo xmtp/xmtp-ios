a0003d468408e5ec21814b0deb6cabe051dc2c59
fabri <fguespe@gmail.com>
2023-12-06 16:04:36 -0300

    init

---
+++ Sources/YourTestTarget/Test.swift
@@ -0,0 +1,7 @@
+/// This is a test class for documentation.
+public class TestClass {
+    /// This method does something.
+    public func doSomething() {
+        print("Doing something")
+    }
+}
