179b90abeede9b0d594532574cc1e6064810f5db
Naomi Plasterer <naomi@xmtp.com>
2025-07-29 17:57:29 -0600

    get on latest version

--- Sources/XMTPiOS/Conversations.swift
+++ Sources/XMTPiOS/Conversations.swift
@@ -72,7 +72,7 @@ actor FfiStreamActor {
 }
 
 /// Handles listing and creating Conversations.
-public actor Conversations {
+public struct Conversations {
 	var client: Client
 	var ffiConversations: FfiConversations
 	var ffiClient: FfiXmtpClient
