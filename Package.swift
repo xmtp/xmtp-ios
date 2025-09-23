c0ee48515c5d5665e2dd337b82270d588f5b1165
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2025-09-22 18:03:08 -0700

    Add new bindings version

--- Package.swift
+++ Package.swift
@@ -4,44 +4,45 @@
 import PackageDescription
 
 let package = Package(
-	name: "XMTPiOS",
-	platforms: [.iOS(.v14), .macOS(.v11)],
-	products: [
-		.library(
-			name: "XMTPiOS",
-			targets: ["XMTPiOS"]
-		),
-		.library(
-			name: "XMTPTestHelpers",
-			targets: ["XMTPTestHelpers"]
-		),
-	],
-	dependencies: [
-		.package(url: "https://github.com/bufbuild/connect-swift", exact: "1.0.0"),
-		.package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.4.3"),
-		.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", "1.8.4" ..< "2.0.0")
-	],
-	targets: [
-		.binaryTarget(
-			name: "LibXMTPSwiftFFI",
-			url: "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-1.5.4.0a2e705/LibXMTPSwiftFFI.zip",
-			checksum: "d59392f586a3d80a4f2ba76c22228c48f08828eed18291edadbc8fdbc224d78b"
-		),
-		.target(
-			name: "XMTPiOS",
-			dependencies: [
-				.product(name: "Connect", package: "connect-swift"),
-				"LibXMTPSwiftFFI",
-				.product(name: "CryptoSwift", package: "CryptoSwift"),
-			]
-		),
-		.target(
-			name: "XMTPTestHelpers",
-			dependencies: ["XMTPiOS"]
-		),
-		.testTarget(
-			name: "XMTPTests",
-			dependencies: ["XMTPiOS", "XMTPTestHelpers"]
-		),
-	]
+    name: "XMTPiOS",
+    platforms: [.iOS(.v14), .macOS(.v11)],
+    products: [
+        .library(
+            name: "XMTPiOS",
+            targets: ["XMTPiOS"]
+        ),
+        .library(
+            name: "XMTPTestHelpers",
+            targets: ["XMTPTestHelpers"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bufbuild/connect-swift", exact: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.4.3"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", "1.8.4"..<"2.0.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "LibXMTPSwiftFFI",
+            url:
+                "https://github.com/xmtp/libxmtp/releases/download/swift-bindings-1.5.4.0a2e705/LibXMTPSwiftFFI.zip",
+            checksum: "d59392f586a3d80a4f2ba76c22228c48f08828eed18291edadbc8fdbc224d78b"
+        ),
+        .target(
+            name: "XMTPiOS",
+            dependencies: [
+                .product(name: "Connect", package: "connect-swift"),
+                "LibXMTPSwiftFFI",
+                .product(name: "CryptoSwift", package: "CryptoSwift"),
+            ]
+        ),
+        .target(
+            name: "XMTPTestHelpers",
+            dependencies: ["XMTPiOS"]
+        ),
+        .testTarget(
+            name: "XMTPTests",
+            dependencies: ["XMTPiOS", "XMTPTestHelpers"]
+        ),
+    ]
 )
