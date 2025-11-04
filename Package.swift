edf015f44816c92ba46b9d12ad7d9df91ce4fabd
cameronvoell <cameronvoell@users.noreply.github.com>
2025-11-04 15:32:01 -0800

    fix url

--- Package.swift
+++ Package.swift
@@ -25,8 +25,7 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "LibXMTPSwiftFFI",
-			url:
-			"https://github.com/xmtp/libxmtp/releases/download/swift-bindings-1.6.0-dev.3656d63/LibXMTPSwiftFFI.zip",
+			url:"https://github.com/xmtp/libxmtp/releases/download/swift-bindings-1.6.1-rc2.5597847/LibXMTPSwiftFFI.zip",
 			checksum: "760d953d565f7070e76941b955647118f21950f3877d80d1ae9395f88bef0d8c"
 		),
 		.target(
